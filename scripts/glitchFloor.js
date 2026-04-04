3083bd2fe1dd876660514aa6aa0092fa5e455bd8
R99space99(Tweu/tinnaphat) <r99.99yt@gmail.com>
2026-04-04 11:30:46 +0700

    Update glitchFloor.js

--- scripts/glitchFloor.js
+++ scripts/glitchFloor.js
@@ -1,12 +1,12 @@
 var GlitchFloor = extend(Floor, {});
-GlitchFloor.region = Core.atlas.find("glitch_0");
+GlitchFloor.region = Core.atlas.find("glitches/glitch_0");
 GlitchFloor.frames = [
-    Core.atlas.find("glitch_0"),
-    Core.atlas.find("glitch_1"),
-    Core.atlas.find("glitch_2")
+    Core.atlas.find("glitches/glitch_0"),
+    Core.atlas.find("glitches/glitch_1"),
+    Core.atlas.find("glitches/glitch_2")
 ];
 GlitchFloor.frameSpeed = 5;
 GlitchFloor.solid = false;
 
 Vars.content.add(GlitchFloor);
-print('Glitch floor loaded :)')
+print('Glitch floor loaded :)');
