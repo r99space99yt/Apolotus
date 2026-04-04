ea01528241b0f2fc387b8777cbedcd87fc479345
R99space99(Tweu/tinnaphat) <r99.99yt@gmail.com>
2026-04-04 11:25:11 +0700

    Create glitchFloor.js

---
+++ scripts/glitchFloor.js
@@ -0,0 +1,11 @@
+var GlitchFloor = extend(Floor, {});
+GlitchFloor.region = Core.atlas.find("glitch_0");
+GlitchFloor.frames = [
+    Core.atlas.find("glitch_0"),
+    Core.atlas.find("glitch_1"),
+    Core.atlas.find("glitch_2")
+];
+GlitchFloor.frameSpeed = 5;
+GlitchFloor.solid = false;
+
+Vars.content.add(GlitchFloor);
