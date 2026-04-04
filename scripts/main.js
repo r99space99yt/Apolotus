456384d8332a499ec34b45fd1175f906a7c3baf9
R99space99(Tweu/tinnaphat) <r99.99yt@gmail.com>
2026-04-04 10:00:01 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,3 @@
 print("LOADER STARTED");
-try {
-    require('suit');
-} catch(e){
-    print("Suit script failed:", e);
-}
-
-try {
-    require('blackholePull');
-} catch(e){
-    print("Blackhole script failed:", e);
-}
+require('suit');
+require('blackholePull');
