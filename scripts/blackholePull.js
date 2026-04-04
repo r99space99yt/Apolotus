3aa65315c5f10b02c2fb895db79973c0bfc7b8fa
R99space99(Tweu/tinnaphat) <r99.99yt@gmail.com>
2026-04-04 11:50:58 +0700

    Update blackholePull.js

--- scripts/blackholePull.js
+++ scripts/blackholePull.js
@@ -117,7 +117,7 @@ function setupBlackholePullLoop(){
                         if(tile != null && tile.floor() != null){
                             if(Math.random() < 0.005){
                                 try {
-                                    tile.setFloor(Blocks.GlitchFloor);
+                                    tile.setFloor(GlitchFloor);
                                 } catch(e){
                                     print("Floor corrupt fail: " + e);
                                 }
